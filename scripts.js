db746782a9762dd10f336980ee61f1cb79933955
Vitor Santos <vg452004@gmail.com>
2025-02-21 18:10:08 -0300

    Projetos adicionados

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-alert('Opa, deu bom!')
+function sendMessage(event){
+    event.preventDefault()
+
+    const name = document.getElementById('nome').value
+    const input_msg = document.getElementById('mensagem').value
+    const telephone = '5515997592983'
+    
+    const msg = `Olá! Meu nome é ${name}.
+    ${input_msg}`
+    const msg_formatted = encodeURIComponent(msg)
+    const url = `https://wa.me/${telephone}?text=${msg_formatted}`
+
+    window.open(url, '_blank')
+
+}
